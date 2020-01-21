fdc72b81b237263100f3b143483ad3964681b7c9
Priyansha Sharma <er.21priyansha@gmail.com>
2020-01-21 15:56:53 +0530

    Added models and admin

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class StockPrice(models.Model):
+    close = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    last = models.FloatField()
+    ttq = models.FloatField()
+    turnover = models.FloatField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.close}, {self.high}, {self.low}, {self.last},\
+        {self.ttq}, {self.turnover}, {self.date}"
